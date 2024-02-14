b07a5badf175b053ee1ab5953076468da39f76f3
Ching Him Leung <himhim2001@live.hk>
2024-02-14 13:46:04 -0600

    disable latexdiff in action, it had not work for a while

--- .github/compile.sh
+++ .github/compile.sh
@@ -12,14 +12,14 @@ else
 fi
 
 latexmk ${latex_option} -outdir=build ${MAIN}
-
+: '
 if [ -n "${prev_tag}" ]; then
 	latexdiff-vc --git -r ${prev_tag} ${MAIN}.tex  --flatten
 	diffName=${MAIN}-diff${prev_tag}
 	echo ${diffName}
 	latexmk ${latex_option} -outdir=build ${diffName}
 fi
-
+'
 if [ -n "${outName}" ]; then
 	mv build/${MAIN}.pdf build/$outName.pdf
 fi
